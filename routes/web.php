268b45019b01170a61a389ac369386cbd4d764aa
Daouda Lo <daoudalothiemping@gmail.com>
2022-09-27 13:08:35 +0000

    init

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Http\Controllers\TweetController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
+use App\Http\Controllers\TweetController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,22 +15,19 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
-});
 
-Route::get('tweets', [TweetController::class, 'index'])->name('tweets.index');
+Route::group(['middleware' => ['auth:sanctum', 'verified']], function () {
+    Route::get('/dashboard', fn () => Inertia\Inertia::render('Dashboard'))->name('dashboard');
+
+    Route::get('/tweets', [TweetController::class, 'index'])->name('tweets.index');
+    Route::post('/tweets', [TweetController::class, 'store'])->name('tweets.store');
+
+    Route::get('/profile/{user:name}', [TweetController::class, 'profile'])->name('tweets.profile');
+
+    Route::get('/followings', [TweetController::class, 'followings'])->name('tweets.followings');
+    Route::post('/unfollows/{user:id}', [TweetController::class, 'unfollows'])->name('tweets.followings.store');
+    Route::post('/follows/{user:id}', [TweetController::class, 'follows'])->name('tweets.followings.store');
+});
